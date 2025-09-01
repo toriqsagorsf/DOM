26bff394de1f8ec04cb0267ab4ec5b5ea7f75c41
toriqsagorsf <toriqsagorsf@gmail.com>
2025-09-01 01:44:53 +0100

    DOM
    Initial commit

---
+++ Test.js
@@ -0,0 +1,6 @@
+//Parent - Child relation
+let grandparent = document.querySelector('.todo-list');
+let children = grandparent.children;
+console.log(children);
+
+
---
+++ es6.js
@@ -0,0 +1,25 @@
+//Variable hoisting.....
+'Use strict'
+name = 'Toriq sagor';
+console.log(name);
+var name;
+
+
+
+
+// Spread operator
+// var poorCity = ['Dhaka','Jhenaidah','Kushtia'];
+// var richCity = ['Wales','London','Rajshahi'];
+// var country = [...poorCity, ...richCity, 'Bangladesh', 'Usa'];
+// console.log(country);
+
+// Rest Parameter
+// function show(a, ... numbers){
+//     let sum = 0;
+//     for(let i of numbers){
+//         sum += i;
+//     }
+//     return sum+a;
+//}
+// let sum = show(10,1,2,3,4) // Add a parameter with rest parameter.
+// console.log(sum)
